4975cef06ce47df7346076b8820ad03e37693a75
Key-System-User <qmhqmhbruh@gmail.com>
2026-08-26 12:48:58 +0300

    Create load.lua

---
+++ load.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Key-System-User/ndag/refs/heads/main/ServerPOS.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Key-System-User/ndag/refs/heads/main/R15D2.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Key-System-User/ndag/refs/heads/main/Align.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Key-System-User/ndag/refs/heads/main/UCOM.lua"))()
